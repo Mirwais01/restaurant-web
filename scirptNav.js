28fbdc169e32412896535aa0fdc13dc1ce3d6919
Mirwais <mirwais.sanaie@gmail.com>
2024-11-21 19:54:32 +0430

    loading animation added.

--- scirptNav.js
+++ scirptNav.js
@@ -1,3 +1,7 @@
 document.querySelector(".first-button").addEventListener("click", function () {
   document.querySelector(".animated-icon1").classList.toggle("open");
 });
+
+window.addEventListener("load", function () {
+  document.body.classList.add("loaded");
+});
