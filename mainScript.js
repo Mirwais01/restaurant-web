261b8d99d5dedff33858e65ae304b26c2406df0e
Mirwais <mirwais.sanaie@gmail.com>
2024-11-25 17:50:44 +0430

    project finishied.

--- mainScript.js
+++ mainScript.js
@@ -6,29 +6,23 @@ window.addEventListener("load", function () {
   document.body.classList.add("loaded");
 });
 
-// active link
 document.addEventListener("DOMContentLoaded", () => {
-  // Select all nav links
   const navLinks = document.querySelectorAll(".nav-link");
 
-  // Normalize the current path (remove trailing slashes, etc.)
   const currentPath = window.location.pathname
     .replace(/\/$/, "")
     .split("/")
     .pop();
 
   navLinks.forEach((link) => {
-    // Normalize the href of the link
     const linkPath = link
       .getAttribute("href")
       .replace(/\/$/, "")
       .split("/")
       .pop();
 
-    // Remove 'active' class from all links
     link.classList.remove("active");
 
-    // Add 'active' class if the paths match
     if (linkPath === currentPath) {
       link.classList.add("active");
     }
---
+++ scriptLogin.js
@@ -0,0 +1,10 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    console.log("Login attempt:", { username, password });
+    alert("Login functionality not implemented in this example.");
+  });
